3a598970f8474e0f48def753d9673a7fce02c30f
xiaoguo <guox652@gmail.com>
2013-08-25 23:40:50 -0400

    Initial Commit

---
+++ wechat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  wechat
+//
+//  Created by Xiao Guo on 8/25/13.
+//  Copyright (c) 2013 Xiao Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
